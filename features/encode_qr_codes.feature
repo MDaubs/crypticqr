1b335a8f752b84e6605e660f905945a60eb998ac
Matthew Daubert <mdaubert@gmail.com>
2011-10-17 19:50:18 -0400

    Finish rounding out Cucumber features

--- features/encode_qr_codes.feature
+++ features/encode_qr_codes.feature
@@ -1,7 +1,15 @@
-Feature: Encode QR codes
+Feature: Encode and decode QR codes
 
   Scenario: A visitor who has set a session password can encode a QR code
-    Given I have set a session password
-    When I go to the home page
-    And  I complete the encode QR code form with the text "Testing 1, 2, 3."
-    Then I should see an encoded and encrypted QR code for the text "Testing 1, 2, 3."
+    Given I have set a session password of "1234"
+    When  I go to the home page
+    And   I complete the encode QR code form with the text "Testing 1, 2, 3."
+    Then  I should see an encoded and encrypted QR code for the text "Testing 1, 2, 3."
+
+  Scenario: A visitor who has not set a session password cannot decode a QR code
+    When  I go to the decode page for a QR code generated with the password "1234" and text "Hello World" with no session password
+    Then  I get an error
+
+  Scenario: A visitor cannot decode a QR code that doesn't match the session password
+    When  I go to the decode page for a QR code generated with the password "1234" and text "Hello World" with the session password "5432"
+    Then  I get an error
---
+++ features/step_definitions/qr_steps.rb
@@ -0,0 +1,37 @@
+Given /^I have set a session password of "([^"]*)"$/ do |session_password|
+  visit root_path
+  fill_in "password", :with => session_password
+  click_button "Set password"
+end
+
+Given /^I have not set a session password$/ do
+end
+
+When /^I go to the decode page for a QR code generated with the password "([^"]*)" and text "([^"]*)" with no session password$/ do |password, plaintext|
+  Given "I have set a session password of \"#{password}\""
+  And   "I complete the encode QR code form with the text \"#{plaintext}\""
+  qr_code = page.find("img")["src"]
+  visit decode_path(qr_code)
+end
+
+When /^I go to the decode page for a QR code generated with the password "([^"]*)" and text "([^"]*)" with the session password "([^"]*)"$/ do |password, plaintext, session_password|
+  Given "I have set a session password of \"#{password}\""
+  And   "I complete the encode QR code form with the text \"#{plaintext}\""
+  qr_code = page.find("img")["src"]
+  And   "I go to the home page"
+  And   "I clear the session password"
+  visit decode_path(qr_code)
+end
+
+When /^I complete the encode QR code form with the text "([^"]*)"$/ do |plaintext|
+  fill_in "p", :with => plaintext
+  click_button "Encode"
+end
+
+Then /^I should see an encoded and encrypted QR code for the text "([^"]*)"$/ do |plaintext|
+  page.should have_selector("img[src*='chart.googleapis.com']")
+end
+
+Then /^I get an error$/ do
+  page.should have_selector("div#alert")
+end
