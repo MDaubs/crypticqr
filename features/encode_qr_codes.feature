cec9187a02d4d07b6b5b8dd171f9163b0b8acdf3
Matthew Daubert <mdaubert@gmail.com>
2011-10-17 20:28:24 -0400

    Proper cucumber feature for successful QR decode

--- features/encode_qr_codes.feature
+++ features/encode_qr_codes.feature
@@ -13,3 +13,7 @@ Feature: Encode and decode QR codes
   Scenario: A visitor cannot decode a QR code that doesn't match the session password
     When  I go to the decode page for a QR code generated with the password "1234" and text "Hello World" with the session password "5432"
     Then  I get an error
+
+  Scenario: A visitor who has set a session password can decode a QR code
+    When  I go to the decode page for a QR code generated with the password "1234" and text "Hello World" with the session password "1234"
+    Then  I see "Hello World"
--- features/step_definitions/qr_steps.rb
+++ features/step_definitions/qr_steps.rb
@@ -10,22 +10,29 @@ end
 When /^I go to the decode page for a QR code generated with the password "([^"]*)" and text "([^"]*)" with no session password$/ do |password, plaintext|
   Given "I have set a session password of \"#{password}\""
   And   "I complete the encode QR code form with the text \"#{plaintext}\""
-  qr_code = page.find("img")["src"]
-  visit decode_path(qr_code)
+  qr_code = page.find("div.ciphertext pre").text
+  And   "I go to the home page"
+  And   "I clear the session password"
+  visit decode_path(:c => qr_code)
 end
 
 When /^I go to the decode page for a QR code generated with the password "([^"]*)" and text "([^"]*)" with the session password "([^"]*)"$/ do |password, plaintext, session_password|
   Given "I have set a session password of \"#{password}\""
   And   "I complete the encode QR code form with the text \"#{plaintext}\""
-  qr_code = page.find("img")["src"]
+  qr_code = page.find("div.ciphertext pre").text
   And   "I go to the home page"
   And   "I clear the session password"
-  visit decode_path(qr_code)
+  And   "I have set a session password of \"#{session_password}\""
+  visit decode_path(:c => qr_code)
 end
 
 When /^I complete the encode QR code form with the text "([^"]*)"$/ do |plaintext|
   fill_in "p", :with => plaintext
   click_button "Encode"
+end
+
+Then /^I see "([^"]*)"$/ do |what_to_see|
+  page.should have_content(what_to_see)
 end
 
 Then /^I should see an encoded and encrypted QR code for the text "([^"]*)"$/ do |plaintext|
