0e1d67dcc51649d3cabff85b335078978002a9bb
Matthew Daubert <mdaubert@gmail.com>
2011-10-22 17:17:58 -0400

    Add mobile.css to the pipeline manifest

--- config/application.rb
+++ config/application.rb
@@ -47,6 +47,7 @@ module Crypticqr
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['mobile.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
