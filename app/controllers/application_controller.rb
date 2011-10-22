e9cc77b9ebb5ac93a9185b0cc78e92ceba524608
Matthew Daubert <mdaubert@gmail.com>
2011-10-22 17:04:31 -0400

    Break mobile styles into their own file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :mobile_device?
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/ || params[:mobile] == "1"
+  end
 end
